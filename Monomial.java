6fc33e0d1bf273281d7346df7475911c807fa488
vinodkri1 <68722669+vinodkri1@users.noreply.github.com>
2022-01-22 15:02:12 -0500

    Update Monomial.java

--- Monomial.java
+++ Monomial.java
@@ -1,16 +1,21 @@
 import java.util.HashSet;
 import java.util.Set;
+
+//Class that stores monomials to be represented as a polynomial
 public class Monomial
 {
     public Set<Integer> vars;
     public int coeff;
 
+    //Constructor for monomial from set of indexed variables
+    //Initialize with a coefficient for the monomial
     public Monomial(Set<Integer> myVars, int myCoeff)
     {
         vars = myVars;
         coeff = myCoeff;
     }
     
+    //Multiply two monomials to form another larger monomial
     public Monomial multiply(Monomial m2)
     {
         int newcoeff = this.coeff * m2.coeff;
@@ -22,6 +27,8 @@ public class Monomial
         return new Monomial(newset, newcoeff);
     }
     
+    //Test whether this monomial matches another (sets of variables are equal)
+    //Coefficients are not necessarily equal
     public boolean match(Monomial m2)
     {
         if (this.vars.containsAll(m2.vars) && m2.vars.containsAll(this.vars))
